6227a346a7ffdba7468cb4c90b73076992810267
배범주 <bbjskybluy@gmail.com>
2019-09-29 16:06:58 +0900

    var 를 const, let 으로 변경, function 화살표함수로 변경, 객체내 메소드선언방식 변경, prettier 플러그인 적용

--- BaeBeomjoo1902/functions/index.js
+++ BaeBeomjoo1902/functions/index.js
@@ -1,75 +1,87 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const moment = require('moment');
-require('moment-timezone');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const moment = require("moment");
+require("moment-timezone");
 moment.tz.setDefault("Asia/Seoul");
 admin.initializeApp();
 
 exports.addToDoList = functions.https.onCall((data, context) => {
+  const name = data.name;
+  const text = data.text;
+  const priority = data.priority;
+  if (!(typeof name === "string") || name.length === 0) {
+    throw new functions.https.HttpsError("invalid-argument", "invalid name");
+  }
+  if (!(typeof text === "string") || text.length === 0) {
+    throw new functions.https.HttpsError("invalid-argument", "invalid text");
+  }
+  if (!(typeof priority === "string") || priority < 1 || priority > 3) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "invalid priority"
+    );
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called " + "while authenticated."
+    );
+  }
 
-    const name = data.name;
-    const text = data.text;
-    const priority = data.priority;
-    if (!(typeof name === 'string') || name.length === 0) {
-        throw new functions.https.HttpsError('invalid-argument', 'invalid name');
-    }
-    if (!(typeof text === 'string') || text.length === 0) {
-        throw new functions.https.HttpsError('invalid-argument', 'invalid text');
-    }
-    if (!(typeof priority === 'string') || priority < 1 || priority > 3) {
-        throw new functions.https.HttpsError('invalid-argument', 'invalid priority');
-    }
-    if (!context.auth) {
-        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-            'while authenticated.');
-    }
+  const date = moment().format("YYYY.MM.DD");
+  const uid = context.auth.uid;
 
-    const date = moment().format('YYYY.MM.DD');
-    const uid = context.auth.uid;
-
-    return admin.database().ref('/todolist/' + uid).push({
-        name: name,
-        text: text,
+  try {
+    admin
+      .database()
+      .ref("/todolist/" + uid)
+      .push({
+        name,
+        text,
         state: "TODO",
-        date: date,
-        priority: priority
-    }).then(() => {
-        return true;
-    })
-        .catch((error) => {
-            throw new functions.https.HttpsError('unknown', error.message, error);
-        });
+        date,
+        priority
+      });
+    return true;
+  } catch (error) {
+    throw new functions.https.HttpsError("unknown", error.message, error);
+  }
 });
 
 exports.moveToRight = functions.https.onCall((data, context) => {
+  const key = data.key;
+  const state = data.state;
 
-    const key = data.key;
-    const state = data.state;
+  if (!(typeof key === "string") || key.length === 0) {
+    throw new functions.https.HttpsError("invalid-argument", "invalid key");
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called " + "while authenticated."
+    );
+  }
+  const uid = context.auth.uid;
 
-    if (!(typeof key === 'string') || key.length === 0) {
-        throw new functions.https.HttpsError('invalid-argument', 'invalid key');
-    }
-    if (!context.auth) {
-        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
-            'while authenticated.');
-    }
-    const uid = context.auth.uid;
+  let nextState = "";
+  if (state === "TODO") {
+    nextState = "DOING";
+  } else if (state === "DOING") {
+    nextState = "DONE";
+  } else {
+    return false;
+  }
 
-    var nextState = "";
-    if (state === "TODO") {
-        nextState = "DOING";
-    } else if (state === "DOING") {
-        nextState = "DONE";
-    } else {
-        return false;
-    }
-
-    return admin.database().ref('/todolist/' + uid + '/' + key).update({
-        state: nextState
-    }).then(() => {
-        return true;
+  return admin
+    .database()
+    .ref("/todolist/" + uid + "/" + key)
+    .update({
+      state: nextState
     })
-        .catch((error) => {
-            throw new functions.https.HttpsError('unknown', error.message, error);
-        });
+    .then(() => {
+      return true;
+    })
+    .catch(error => {
+      throw new functions.https.HttpsError("unknown", error.message, error);
+    });
 });
--- BaeBeomjoo1902/public/main.js
+++ BaeBeomjoo1902/public/main.js
@@ -1,138 +1,138 @@
+window.onload = () => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      tableContents.logined = true;
 
+      const userId = firebase.auth().currentUser.uid;
 
-window.onload = function () {
+      firebase
+        .database()
+        .ref("todolist/" + userId)
+        .on("child_added", data => {
+          const resultObject = {
+            name: data.val().name,
+            text: data.val().text,
+            state: data.val().state,
+            priority: data.val().priority,
+            date: data.val().date,
+            key: data.key
+          };
+          app.todoList.push(resultObject);
+        });
 
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            tableContents.logined = true;
-
-            var userId = firebase.auth().currentUser.uid;
-
-            firebase.database().ref('todolist/' + userId).on('child_added', function (data) {
-
-                var resultObject = {
-                    name: data.val().name,
-                    text: data.val().text,
-                    state: data.val().state,
-                    priority: data.val().priority,
-                    date: data.val().date,
-                    key: data.key,
-                }
-                app.todoList.push(resultObject);
-            });
-
-            firebase.database().ref('todolist/' + userId).on('child_changed', function (data) {
-                console.log("child_changed: " + data.key);
-                for(var i=0;i<app.todoList.length;i++){
-                    if(app.todoList[i].key === data.key){
-                        app.todoList[i].state = data.val().state;
-                        break;
-                    }
-                }
-            });
-
-
-        } else {
-            tableContents.logined = false;
-            app.todoList = [];
-        }
-    });
+      firebase
+        .database()
+        .ref("todolist/" + userId)
+        .on("child_changed", data => {
+          for (let i = 0; i < app.todoList.length; i++) {
+            if (app.todoList[i].key === data.key) {
+              app.todoList[i].state = data.val().state;
+              break;
+            }
+          }
+        });
+    } else {
+      tableContents.logined = false;
+      app.todoList = [];
+    }
+  });
 };
 
+const tableContents = {
+  logined: false,
 
-var tableContents = {
+  isButtonDisabled: false,
+  isAddToDoMenu: false,
 
-    logined: false,
+  states: ["TODO", "DOING", "DONE"],
 
-    isButtonDisabled: false,
-    isAddToDoMenu: false,
+  sendObject: {
+    text: "",
+    priority: "1",
+    name: ""
+  },
 
-    states: ["TODO", "DOING", "DONE"],
+  todoList: []
+};
 
-    sendObject: {
+const app = new Vue({
+  el: "#app",
+  data: tableContents,
+  methods: {
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider);
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    clickAddTodo() {
+      this.isAddToDoMenu = true;
+    },
+    backToTable() {
+      this.isAddToDoMenu = false;
+    },
+    removeToDoList() {
+      this.sendObject = {
         text: "",
         priority: "1",
         name: ""
+      };
     },
+    addToDoList() {
+      this.isButtonDisabled = true;
 
-    todoList: [
-    ]
+      const addToDoList = firebase.functions().httpsCallable("addToDoList");
+      addToDoList(this.sendObject)
+        .then(result => {
+          this.isButtonDisabled = false;
+          this.backToTable();
+          this.removeToDoList();
+        })
+        .catch(error => {
+          const { code, message, details } = error;
 
-}
+          console.error(
+            "There was an error when calling the Cloud Function",
+            error
+          );
+          window.alert(
+            "There was an error when calling the Cloud Function:\n\nError Code: " +
+              code +
+              "\nError Message:" +
+              message +
+              "\nError Details:" +
+              details
+          );
+          this.isButtonDisabled = false;
+          this.backToTable();
+          this.removeToDoList();
+        });
+    },
+    moveToRight(key, state) {
+      this.isButtonDisabled = true;
+      const moveToRight = firebase.functions().httpsCallable("moveToRight");
+      moveToRight({ key: key, state: state })
+        .then(result => {
+          this.isButtonDisabled = false;
+        })
+        .catch(error => {
+          const { code, message, details } = error;
 
-var app = new Vue({
-    el: '#app',
-    data: tableContents,
-    methods: {
-        login: function () {
-            var provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider);
-        },
-        logout: function () {
-            firebase.auth().signOut();
-        },
-        clickAddTodo: function () {
-            this.isAddToDoMenu = true;
-        },
-        backToTable: function () {
-            this.isAddToDoMenu = false;
-        },
-        removeToDoList: function () {
-            this.sendObject = {
-                text: "",
-                priority: "1",
-                name: ""
-            }
-        },
-        addToDoList: function () {
-
-            this.isButtonDisabled = true;
-            console.log(this.sendObject);
-            var addToDoList = firebase.functions().httpsCallable('addToDoList');
-            addToDoList(this.sendObject).then(function (result) {
-
-                this.isButtonDisabled = false;
-                this.backToTable();
-                this.removeToDoList();
-                console.log(result);
-
-            }.bind(this)).catch(function (error) {
-
-                var code = error.code;
-                var message = error.message;
-                var details = error.details;
-                
-                console.error('There was an error when calling the Cloud Function', error);
-                window.alert('There was an error when calling the Cloud Function:\n\nError Code: '
-                    + code + '\nError Message:' + message + '\nError Details:' + details);
-                this.isButtonDisabled = false;
-                this.backToTable();
-                this.removeToDoList();
-
-            }.bind(this));
-
-        },
-        moveToRight: function (key, state) {
-
-            this.isButtonDisabled = true;
-            var moveToRight = firebase.functions().httpsCallable('moveToRight');
-            moveToRight({ key: key, state: state }).then(function (result) {
-
-                console.log(result);
-                this.isButtonDisabled = false;
-
-            }.bind(this)).catch(function (error) {
-                
-                var code = error.code;
-                var message = error.message;
-                var details = error.details;
-                
-                console.error('There was an error when calling the Cloud Function', error);
-                window.alert('There was an error when calling the Cloud Function:\n\nError Code: '
-                    + code + '\nError Message:' + message + '\nError Details:' + details);
-                this.isButtonDisabled = false;
-
-            }.bind(this));
-        },
+          console.error(
+            "There was an error when calling the Cloud Function",
+            error
+          );
+          window.alert(
+            "There was an error when calling the Cloud Function:\n\nError Code: " +
+              code +
+              "\nError Message:" +
+              message +
+              "\nError Details:" +
+              details
+          );
+          this.isButtonDisabled = false;
+        });
     }
-})
+  }
+});
